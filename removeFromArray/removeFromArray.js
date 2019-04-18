2c42ff2ca28194d17792e9870d45f0564e6a2e1e
caizhang <justStarNew@gmail.com>
2019-04-18 19:01:08 +0800

    commit my soluation

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
-
+    let result = [...arguments[0]];
+    for (var i = 1; i < arguments.length; i++) {
+       result = result.filter(function(value,index,result) {
+        return value != arguments[i];
+      });
+    }
+    return result;
 }
 
 module.exports = removeFromArray
