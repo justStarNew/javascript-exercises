1257163a3bc4ed11897c0c1b6f2de964c1caf4d7
caizhang <justStarNew@gmail.com>
2019-04-18 19:02:00 +0800

    commit my solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let result = "";
+    if (num > 0) {
+      for (var i = 0; i <num;i++) {
+        result += str;
+      }
+    } else if (num < 0) {
+        result = "ERROR";
+    }
+    return result;
 }
 
 module.exports = repeatString
+  
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let arr = string.split("");
+  let result = "";
+  for (var i = arr.length - 1; i >=0 ; i--) {
+    result += arr[i];
+  }
+  return result;
 }
 
 module.exports = reverseString
