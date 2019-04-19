6b25d3e0361400f024a1ce8b078572c6d36ee325
caizhang <justStarNew@gmail.com>
2019-04-19 10:17:38 +0800

    commit leapyear test

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result = false;
+    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+      result = true;
+    }
+    return result;
 }
 
 module.exports = leapYears
