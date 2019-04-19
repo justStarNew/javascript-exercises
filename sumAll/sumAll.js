998d0da9d292b96df721b552f92fe1e2f17586cc
caizhang <justStarNew@gmail.com>
2019-04-19 10:07:10 +0800

    modify sumall

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(num1,num2) {
   let result = 0;
-  if(isNaN(num2) || isNaN(num1) || num1 < 0 || num2 < 0) {
+  if(typeof(num2) != "number" || typeof(num1) != "number" || num1 < 0 || num2 < 0) {
       result = "ERROR";
   } else {
       for(let i = Math.min(num1,num2);i<=Math.max(num1,num2);i++) {
