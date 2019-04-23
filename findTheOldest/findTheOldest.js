aef19a4ee822bcab399c537d36a2f4fd45064b40
caizhang <justStarNew@gmail.com>
2019-04-23 19:36:21 +0800

    add find the oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+      return people.reduce((result,curr) => {
+        //result save the oldest people
+          let age1 = Number(result.yearOfDeath?result.yearOfDeath:new Date().getFullYear()) - Number(result.yearOfBirth);
+          let age2 = Number(curr.yearOfDeath?curr.yearOfDeath:new Date().getFullYear()) - Number(curr.yearOfBirth);
+          if(age1 > age2) {
+              return result;
+          } else {
+            return curr;
+          }
+      })
 }
 
 module.exports = findTheOldest
