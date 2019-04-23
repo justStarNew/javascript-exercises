f54a6ece973e903229c81703325fe7b27370890f
caizhang <justStarNew@gmail.com>
2019-04-23 11:17:37 +0800

    add palindromes

---
+++ dom_manipulation/index.js
@@ -0,0 +1,6 @@
+const buttons = document.querySelectorAll("button")
+buttons.forEach((button) => {
+  button.addEventListener("click",(e) => {
+  alert(button.id);
+});
+});
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function() {
+const palindromes = function(s) {
+    //reverse the string,if both are equal then it is a palindromes
+    let transformeds = s.replace(/[ ,.!]/g,"");
+    if(transformeds.toUpperCase() === reverseString(transformeds).toUpperCase()) 
+      return true;
+    else
+      return false;
+      
+}
 
+let reverseString = function(s) {
+    if (s === "") 
+      return "";
+    else
+      return reverseString(s.substr(1)) + s.charAt(0);
 }
 
 module.exports = palindromes
