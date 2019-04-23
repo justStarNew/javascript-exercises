753686bb005a2f5b1e9a2f1849acb8f7df71b616
caizhang <justStarNew@gmail.com>
2019-04-23 19:01:22 +0800

    commit getthetitle

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+      return arr.reduce((result,current) => [...result,current.title],[])
 }
 
 module.exports = getTheTitles;
